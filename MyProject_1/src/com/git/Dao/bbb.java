e2fbf3cac3ad3dac648b2bc89f6e11cc0c5386bb
Dileep 123 <Dileep 123@Dileep>
2023-10-06 12:35:41 +0530

    TL Update BBB Class And Commit To CR

--- MyProject_1/src/com/git/Dao/bbb.java
+++ MyProject_1/src/com/git/Dao/bbb.java
@@ -4,7 +4,12 @@ public class bbb {
 
 	public static void main(String[] args) {
 	System.out.println("Hii iam Div2 project BBB class");
-
+       ggg();
 	}
+	
+	public static void ggg() {
+		System.out.println("Hii iam Div2 project GGG method in BBB class");
+
+		}
 
 }
