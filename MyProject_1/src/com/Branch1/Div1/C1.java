8bfa110eb1ceefcea86517f5f45090116bc9d485
Dileep 123 <Dileep 123@Dileep>
2023-10-06 16:36:31 +0530

    C1.m1()..creating Dv1 ..

---
+++ MyProject_1/src/com/Branch1/Div1/C1.java
@@ -0,0 +1,13 @@
+package com.Branch1.Div1;
+
+public class C1 {
+
+	public static void main(String[] args) {
+		
+
+	}
+	public void m1(){
+		System.out.println("C1.m1()..creating Dv1 ..");
+	}
+
+}
