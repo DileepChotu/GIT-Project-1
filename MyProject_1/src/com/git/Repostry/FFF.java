5807101d019fa693117959caf438950f4e918ebb
Dileep 123 <Dileep 123@Dileep>
2023-10-06 15:32:50 +0530

    TL Creating DDD and FFF Class and Commit CR

---
+++ MyProject_1/src/com/git/Repostry/FFF.java
@@ -0,0 +1,10 @@
+package com.git.Repostry;
+
+public class FFF {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ MyProject_1/src/com/git/Servece/DDD.java
@@ -0,0 +1,10 @@
+package com.git.Servece;
+
+public class DDD {
+
+	public static void main(String[] args) {
+       
+
+	}
+
+}
