aa7a459eb4320423502a1827c09ea13ad55c99f2
Dileep 123 <Dileep 123@Dileep>
2023-10-06 12:42:28 +0530

    AAA Method Udate in BBB class From Tl

--- MyProject_1/src/com/git/Dao/bbb.java
+++ MyProject_1/src/com/git/Dao/bbb.java
@@ -8,8 +8,12 @@ public class bbb {
 	}
 	
 	public static void ggg() {
-		System.out.println("Hii iam Div2 project GGG method in BBB class");
+		System.out.println("Hii iam Div2 project GGG method in BBB class upadte in TL");
 
 		}
+	public static void AAA() {
+		System.out.println("Hii iam BBB Class project AAA method in  upadte in TL");
+
+	}
 
 }
